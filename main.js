8935773f264f4c584032fe0d96230d11be1198f8
Patryk Mielczarek <patryk.mielczarek99@gmail.com>
2022-12-06 18:28:54 +0100

    Finish

--- main.js
+++ main.js
@@ -1,102 +1,3 @@
-// const input = document.querySelector(".search-bar");
-// const button = document.querySelector(".search button");
-//
-// let cities = [];
-//
-// let weather = {
-//     apiKey: "f5bdb001418c0e448f214340aaa98c11",
-//     fetchWeather: function (city) {
-//         fetch(
-//             "https://api.openweathermap.org/data/2.5/weather?q=" +
-//                 city +
-//                 "&units=metric&appid=" +
-//                 this.apiKey
-//         )
-//             .then((response) => response.json())
-//             .then((data) => this.displayWeather(data));
-//     },
-//     displayWeather: function (data) {
-//         const { name } = data;
-//         const { icon, main } = data.weather[0];
-//         const { temp } = data.main;
-//         if(cities.includes(name)){
-//             cities.push({
-//                 name: name,
-//                 icon: icon,
-//                 main: main,
-//                 temp: temp,
-//             });
-//
-//             document.querySelector(".city").innerText = name;
-//             document.querySelector(".temp").innerText = `${temp.toFixed(0)}°C`;
-//             document.querySelector(".icon").src =
-//                 "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-//             document.querySelector(".main").innerText = main;
-//             console.log(cities);        }
-//     },
-//     search: function () {
-//         this.fetchWeather(input.value);
-//     },
-// };
-//
-// const duplicateChildNodes = () => {
-//     const elem = document.getElementById("card");
-//     const copy = elem.cloneNode(true);
-//     elem.parentNode.insertBefore(copy, elem);
-// };
-//
-// button.addEventListener("click", function () {
-//     if (input.value === "" || search === 0) {
-//         Toastify({
-//             text: "Type name of city",
-//             offset: {
-//                 x: 50, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-//                 y: 10, // vertical axis - can be a number or a string indicating unity. eg: '2em'
-//             },
-//             style: {
-//                 background: "red",
-//             },
-//         }).showToast();
-//     } else {
-//         weather.search();
-//         duplicateChildNodes();
-//     }
-//     return;
-// });
-//
-// input.addEventListener("keyup", function () {
-//     if (input.value === "") {
-//         Toastify({
-//             text: "Type name of city",
-//             offset: {
-//                 x: 50, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-//                 y: 10, // vertical axis - can be a number or a string indicating unity. eg: '2em'
-//             },
-//             style: {
-//                 background: "red",
-//             },
-//         }).showToast();
-//     } else if (event.key == "Enter") {
-//         weather.search();
-//         duplicateChildNodes();
-//     }
-//     return;
-// });
-//
-// weather.fetchWeather("London");
-
-
-// fetchWeather: function (city) {
-//     fetch(
-//         "https://api.openweathermap.org/data/2.5/weather?q=" +
-//         city +
-//         "&units=metric&appid=" +
-//         this.apiKey
-//     )
-//         .then((response) => response.json())
-//         .then((data) => this.displayWeather(data));
-// },
-
 const input = document.querySelector(".search-bar");
 const button = document.querySelector(".search button");
 const cityList = [];
@@ -114,50 +15,54 @@ input.addEventListener("keyup", async function () {
             },
         }).showToast();
     } else if (event.key === "Enter") {
-        await handleSearch(input.value)
+        await handleSearch(input.value);
     }
 });
 
 const deleteCity = (id) => {
-    const card = document.querySelector(`[data-id="${id}"]`)
+    const card = document.querySelector(`[data-id="${id}"]`);
     card.remove();
     cityList.splice(id, 1);
-}
+};
 
 const cardElement = (city) => `<div id="card" data-id="${city.id}">
                 <h2 class="city">${city.name}</h2>
                 <div class="temp">${city.temp.toFixed(0)}°C</div>
-                <img class="icon" src="${"http://openweathermap.org/img/wn/" + city.icon + "@2x.png"}" alt="" />
+                <img class="icon" src="${
+                    "http://openweathermap.org/img/wn/" + city.icon + "@2x.png"
+                }" alt="" />
                 <div class="main">${city.main}</div>
-                <button class="delete" href="#" onclick="deleteCity(${city.id})">Delete</button>
-                    <ion-icon name="trash-outline"></ion-icon
+                <button class="delete" href="#" onclick="deleteCity(${
+                    city.id
+                })"><ion-icon name="trash-outline"></ion-icon</button>
+                    
                 >
-            </div>`
+            </div>`;
 
-
-const checkCityExist = (data) => cityList.filter((city) => {
-    return city.name === data.name
-}).length === 0
-
-
+const checkCityExist = (data) =>
+    cityList.filter((city) => {
+        return city.name === data.name;
+    }).length === 0;
 
 const handleSearch = async (city) => {
     await fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        "f5bdb001418c0e448f214340aaa98c11"
+            city +
+            "&units=metric&appid=" +
+            "f5bdb001418c0e448f214340aaa98c11"
     )
         .then((response) => response.json())
         .then((data) => {
             if (checkCityExist(data)) {
-                const {name} = data;
-                const {icon, main} = data.weather[0];
-                const {temp} = data.main;
-                const city = {name, icon, main, temp,};
+                const { name } = data;
+                const { icon, main } = data.weather[0];
+                const { temp } = data.main;
+                const city = { name, icon, main, temp };
                 cityList.push(city);
                 const card = cardElement(city);
-                document.querySelector(".cardwrapper").insertAdjacentHTML("beforeend", card);
+                document
+                    .querySelector(".cardwrapper")
+                    .insertAdjacentHTML("beforeend", card);
             } else {
                 Toastify({
                     text: "City already exists",
@@ -171,13 +76,10 @@ const handleSearch = async (city) => {
                 }).showToast();
             }
         });
-}
+};
 
 button.addEventListener("click", () => handleSearch(input.value));
 
-( () => {
-     handleSearch("London")
+(() => {
+    handleSearch("London");
 })();
-
-
-
